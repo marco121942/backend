f503083ff72964c1bee6b5bf61cf682b0a365025
marco121942 <marco.sotomayor@tecsup.edu.pe>
2020-08-23 16:58:50 -0500

    first commit

---
+++ modelos/rutas.php
@@ -0,0 +1,25 @@
+<?php
+
+class Ruta{
+
+	/*=============================================
+	RUTA LADO DEL CLIENTE
+	=============================================*/	
+
+	public function ctrRuta(){
+
+		return "https://ecommerce-tesis.herokuapp.com/frontend/";
+	
+	}
+
+	/*=============================================
+	RUTA LADO DEL SERVIDOR
+	=============================================*/	
+
+	public function ctrRutaServidor(){
+
+		return "https://backend-tesis.herokuapp.com/backend/";
+	
+	}
+
+}
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,7 @@
+ <!-- main-footer -->
+<footer class="main-footer">
+
+<strong>Copyright &copy; 2010 <a href="#">Monadas Glamour</a>.</strong> Todos los derechos reservados.
+
+</footer>
+<!-- main-footer -->
